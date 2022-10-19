7b53d3d92697d24d969b0004cce2d913faff9fea
Firlya Adya <firlyaadya@gmail.com>
2022-10-19 11:36:22 +0700

    initial commit

---
+++ app/src/main/java/com/example/latihan2/ChoiceOfOffenseActivity.java
@@ -0,0 +1,14 @@
+package com.example.latihan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoiceOfOffenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choice_of_offense_violation);
+    }
+}
